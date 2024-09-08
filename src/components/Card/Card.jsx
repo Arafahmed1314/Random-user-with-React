58cc53e03859027d7e9b829a928024ed76c8212e
Naimul Islam <nayemhasan1314@gmail.com>
2024-09-08 12:07:21 +0600

    add salary

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-export default function Card({ user, img }) {
-  console.log(user, img);
+export default function Card({ user, img, handleUser }) {
 
   // Correct destructuring for city (nested inside address)
   const { name, email, phone, address: { city } } = user;
   const { download_url, height, url } = img || {};
 
   return (
-    <div className="card flex  md:flex-col gap-8 p-4 justify-center items-center w-[400px] bg-gray-800 rounded-md text-white">
+    <div className="card flex md:flex-col gap-8 p-4 justify-center items-center w-[400px] bg-gray-800 rounded-md text-white">
       <div className="card-image">
         {img && <img className='w-[250px] rounded-full' src={download_url} alt={name} />}
       </div>
@@ -17,7 +16,11 @@ export default function Card({ user, img }) {
         <p>Email: {email}</p>
         <p>Phone: {phone}</p>
         <p>City: {city}</p>
-        <button className='mt-2 bg-white text-gray-800 text-md font-semibold py-1 px-2 rounded-md hover:bg-orange-700 duration-500 hover:text-white'>Add Salary</button>
+        <button
+          className='mt-2 bg-white text-gray-800 text-md font-semibold py-1 px-2 rounded-md hover:bg-orange-700 duration-500 hover:text-white'
+          onClick={() => handleUser(img)}>
+          Add Salary
+        </button>
       </div>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import Card from "../Card/Card";
 export default function Home() {
   const [data, setData] = useState([]);
   const [imgData, setImgData] = useState([]);
+  const [rightData, setRightData] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -17,20 +18,37 @@ export default function Home() {
       .then((data) => setImgData(data));
   }, []);
 
+  const handleUser = (img) => {
+    const data = [...rightData, img];
+    setRightData(data);
+    console.log("it works", img);
+  };
+
   return (
-    <div className="pt-8 flex ">
+    <div className="py-8 flex ">
       <div className="left-container md:w-[70%] border-r-2">
         <div className="card-wrapper flex flex-col md:flex-row gap-5 flex-wrap justify-center">
           {data.map((user, index) => (
             <Card
               key={index}
               user={user}
+              handleUser={handleUser}
               img={imgData[index]}
             />
           ))}
         </div>
       </div>
-      <div className="right-container md:w-[30%]"></div>
+      <div className="right-container w-[30%]">
+  <h1 className="bg-gray-800 p-4 text-white text-xl rounded-lg">
+    Total Salary:
+    {rightData.reduce((total, img) => total + img.height, 0)}$
+    <br />
+    <br />
+    Total People Added: {rightData.length}
+  </h1>
+</div>
+
+
     </div>
   );
 }
